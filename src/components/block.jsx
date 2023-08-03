a81c8ebd803bab3c7c93c300d974ebc8cfaf14d0
AI-MHT <aiman.mouhat.etu21@ensem.ac.ma>
2023-08-03 11:39:13 +0100

    updates Departements

--- src/components/block.jsx
+++ src/components/block.jsx
@@ -1,20 +1,19 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom"
 
 const Block = ({ imageUrl, altText, title, linkTo }) => {
-  const imagePath = process.env.PUBLIC_URL + imageUrl;
-
-  return (
-    <div className="card 1">
-      <Link to={linkTo}>
-        <div className="card_image">
-          <img src={imagePath} alt={altText} />
-        </div>
-        <div className="card_title title-white">
-          <div id="demoFont">{title}</div>
-        </div>
-      </Link>
-    </div>
-  );
-};
+    return (
+      <div className="card 1">
+        <Link to={linkTo}>
+          <div className="card_image">
+            <img src={imageUrl} alt={altText} />
+          </div>
+          <div className="card_title title-white">
+            <div id="demoFont">{title}</div>
+          </div>
+        </Link>
+      </div>
+    );
+  };
 
 export default Block;
+  
