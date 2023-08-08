c66a083a3d42d9b8c643d0eed4eb130d0afc39d5
AI-MHT <aiman.mouhat.etu21@ensem.ac.ma>
2023-08-08 14:54:03 +0100

    add server.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
       <section className="banner_main">
         <div className="ContactFormWidget">
           <div className="form">
-            <form action="https://formspree.io/f/xeqbqrvz" method="POST">
+            <form action="http://localhost:3000/" method="POST">
               <div className="formColum">
                 <div className="FormControl">
                   <input id="ContactName" name="name" placeholder="Name" type="text" required />
