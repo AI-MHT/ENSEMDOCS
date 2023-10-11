fdbf2ccfc766352c7367e05a1c1f2813ccc4689b
AI-MHT <aiman.mouhat.etu21@ensem.ac.ma>
2023-10-11 09:45:53 +0100

    updat etable

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import Home from "./pages/Home"
-import Footer from "./components/Footer"
-import AboutUs from "./pages/AboutUs"
+import CVs from "./pages/CVs";
+import ReactGA from 'react-ga';  // Added this import
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AboutUs from "./pages/AboutUs";
 import Departement from "./pages/Departement";
-import ContactUs from "./pages/ContactUs"
-import Gld from "./pages/Gld"
-import Error from "./pages/Error"
-import CVs from "./pages/CVs"
-import {Routes,Route} from 'react-router-dom'
-<<<<<<< HEAD
+import ContactUs from "./pages/ContactUs";
+import Gld from "./pages/Gld";
+import Error from "./pages/Error";
 
-
-=======
-import ReactGA from 'react-ga';
->>>>>>> 794b57bd78aa3be2749f77c5fc715e9d31c0c8ea
 function App() {
   ReactGA.initialize('G-32QL9DC11C');
   ReactGA.pageview(window.location.pathname + window.location.search);
+  
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Gld" element={<Gld/>}/>
-        <Route path="/Departement" element={<Departement />}/>
-        <Route path="/contact" element={<ContactUs />}/>
-        <Route path="/About" element={<AboutUs />}/>
-        <Route path="/CVs" element={<CVs />}/>
-        <Route path="/*" element={<Error />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Gld" element={<Gld />} />
+        <Route path="/Departement" element={<Departement />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/CVs" element={<CVs />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
